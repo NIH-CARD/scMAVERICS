db36175cb2df14194d84e688dd9ed582b0d302bc
eugf <eugf@users.noreply.github.com>
2025-04-02 21:30:47 -0400

    Improve readability

--- snakemake.sh
+++ snakemake.sh
@@ -30,4 +30,7 @@ chmod 777 scripts/cellbender_array.sh
 chmod 777 scripts/atac_model.sh
 
 # RUN SCRIPT
-snakemake --cores all --profile snakemake_profile --use-singularity
+snakemake \
+    --cores all \
+    --profile snakemake_profile \
+    --use-singularity
