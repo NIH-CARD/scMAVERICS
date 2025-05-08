014c6b21c8ed37194661adb452d5330b6586deb0
Adam Catching <adamcatching@gmail.com>
2025-05-08 13:43:07 -0400

    Updated to iterate through samples

--- scripts/cistopic_pseudobulk.py
+++ scripts/cistopic_pseudobulk.py
@@ -19,15 +19,15 @@ samples = snakemake.params.samples
 # For sample in samples
 for i, sample in enumerate(samples):
     # Define fragment file
-    bed_location = fragment_file[i]
+    bed_location = snakemake.input.fragment_file[i]
     # Load fragment with polars
     print(f'Loading sample {sample} fragments')
     pl_fragment = pl.read_csv(bed_location, separator='\t', comment_prefix='#', n_threads=8)
     pl_fragment.columns = ['chrom', 'chromStart', 'chromEnd', 'name', 'score']
     # Get list of sample and cell type specific barcodes
-    cell_type_barcodes = {cell_type: cell_df[(cell_df['cell_type']== cell_type) & (cell_df[sample_value] == sample)]['cell_barcode'].to_list() for cell_type in cell_types}
+    cell_type_barcodes = {cell_type: cell_df[(cell_df['cell_type']== cell_type) & (cell_df[sample_value] == sample)]['cell_barcode'].to_list() for cell_type in snakemake.params.cell_types}
     # Filter on the cell type barcodes
-    cell_fragment = {cell_type: pl_fragment.filter(pl_fragment['name'].is_in(cell_type_barcodes[cell_type])) for cell_type in cell_types}
+    cell_fragment = {cell_type: pl_fragment.filter(pl_fragment['name'].is_in(cell_type_barcodes[cell_type])) for cell_type in snakemake.params.cell_types}
     # Add the filtered barcodes to the fragments
     print(f'Writing sample {sample}')
     for j, cell_type in enumerate(snakemake.params.cell_types):
