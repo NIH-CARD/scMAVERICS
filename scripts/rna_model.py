0b7d14560f534a9e0b725c3a40c9bc6cb60711ba
adamcatching <adamcatching@gmail.com>
2025-07-09 09:26:12 -0400

    Update scVI RNA modeling parameters to reflect split feature selection and modeling steps

--- scripts/rna_model.py
+++ scripts/rna_model.py
@@ -12,39 +12,20 @@ print(torch.cuda.is_available())
 scvi.settings.seed = 0
 torch.set_float32_matmul_precision('high')
 
-print(sys.argv)
-
 # Read in AnnData atlas object
 adata = ad.read_h5ad(sys.argv[1])
 
-# Double check that no transcripts not found in cells are in the atlas
-sc.pp.filter_genes(adata, min_cells=3)
-
-# Select for the most variable genes
-sc.pp.highly_variable_genes(
-    adata, 
-    layer='log-norm',
-    n_top_genes=10000, 
-    batch_key=sys.argv[2])
-
-# Define mitochondria and ribosome genes to remove
-adata.var['mt'] = adata.var_names.str.startswith('MT-')
-adata.var['rb'] = adata.var_names.str.startswith(('RPL', 'RPS'))
-
-# Make a copy of the AnnData atlas that only contains variable genes
-filtered_adata = adata[:, (adata.var['highly_variable']) & ~(adata.var['mt']) & ~(adata.var['rb'])].copy()
-
 # Setup SCVI on the data layer
 scvi.model.SCVI.setup_anndata(
-    filtered_adata, layer="log-norm", batch_key=sys.argv[2])
+    filtered_adata, layer="counts", batch_key=sys.argv[2])
 
 # Add the parameters of the model
 model = scvi.model.SCVI(
     filtered_adata, 
-    dispersion="gene-cell", 
+    dispersion="gene-batch", 
     n_layers=2, 
     n_latent=30, 
-    gene_likelihood="zinb"
+    gene_likelihood="nb"
 )
 
 # Train the model
