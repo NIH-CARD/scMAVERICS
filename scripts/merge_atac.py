cd0a5e0e1633aa3bcbe59165a7ef02407bad7c3f
eugf <eugf@users.noreply.github.com>
2025-08-06 13:48:20 -0400

    (WIP) Some issues fixed, but rule is not needed, stopping here

--- scripts/merge_atac.py
+++ scripts/merge_atac.py
@@ -8,9 +8,9 @@ samples = snakemake.params.samples
 atac_anndata = snakemake.input.atac_anndata
 
 # Import metadata
-metadata = pd.read_csv(snakemake.input.metadata_table)
+metadata_table = pd.read_csv(snakemake.input.metadata_table)
 # Reset the index for the samples
-metadata = metadata_table.set_index(snakefile.params.sample_key)
+metadata_table = metadata_table.set_index(snakemake.params.sample_key)
 
 # Read in snapATAC2 datasets into a list of anndata objects in read only
 list_of_anndata = [(samples[i], snap.read(atac_anndata[i])) for i in range(len(atac_anndata))]
@@ -18,7 +18,8 @@ list_of_anndata = [(samples[i], snap.read(atac_anndata[i])) for i in range(len(a
 # Create the AnnDataSet from the snapATAC2 datasets
 anndataset = snap.AnnDataSet(
     adatas=list_of_anndata,
-    filename=atac_anndata
+    # filename=atac_anndata
+    filename=snakemake.output[0],
 )
 
 # Update identifiers
@@ -28,29 +29,29 @@ for value in metadata_table.columns.to_list():
     # Create a new dictionary for each sample-metadata value
     sample2value = metadata_table[value].to_dict()
     # Assign this value
-    anndataset.obs[new_obs] = [sample2value[x] for x in anndataset.obs[snakefile.params.sample_key].to_list()]
+    anndataset.obs[new_obs] = [sample2value[x] for x in anndataset.obs[snakemake.params.sample_key].to_list()]
 
 # Select variable features
-snap.pp.select_features(adataset, n_features=250000, n_jobs=60)
+snap.pp.select_features(anndataset, n_features=250000, n_jobs=60)
 
 # Spectral MDS analysis
-snap.tl.spectral(adataset)
+snap.tl.spectral(anndataset)
 
 # Batch correction
-snap.pp.mnc_correct(adataset, batch=snakemake.params.sample_key, key_added='X_spectral')
+snap.pp.mnc_correct(anndataset, batch=snakemake.params.sample_key, key_added='X_spectral')
 
 # Compute nearest neighbors from the corrected spectral MDS
-snap.pp.knn(adataset)
+snap.pp.knn(anndataset)
 
 # Compute clusters
-snap.tl.leiden(adataset)
+snap.tl.leiden(anndataset)
 
 # Compute umap
-snap.tl.umap(adataset)
+snap.tl.umap(anndataset)
 
 # Save values
-pd.DataFrame(adataset.obsm['X_umap']).to_csv(snakemake.output.umap_data)
-pd.DataFrame(adataset.var[['count', 'selected']])to_csv(snakemake.output.var_data)
+pd.DataFrame(anndataset.obsm['X_umap']).to_csv(snakemake.output.umap_data)
+pd.DataFrame(anndataset.var[['count', 'selected']]).to_csv(snakemake.output.var_data)
 
 """ THIS AREA FOR INTEGRATING ANNOTATION WITH RNA DATA"""
 # Save the dataframe
