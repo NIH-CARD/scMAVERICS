0f10da553cda3691890343aef994ea3e87cbb65d
Adam Catching <adamcatching@gmail.com>
2026-04-20 14:32:46 -0400

    Fixed filtering by condition, cell type

--- scripts/atac_by_celltype.py
+++ scripts/atac_by_celltype.py
@@ -11,30 +11,43 @@ import pyranges as pr
 sample_key = snakemake.params.sample_key
 seq_batch_key = snakemake.params.seq_batch_key
 disease_param = snakemake.params.disease_param
+disease = snakemake.params.disease
 
 # Read in rna observation data
 rna = sc.read_h5ad(snakemake.input.merged_rna_anndata)
 cell_data = rna.obs
-print(rna.n_obs)
+
+# Check syntax and correct for the disease filter
+if isinstance(disease, list):
+    disease_list = disease
+else:
+    disease_list = [disease]
 
 # Get cell barcodes of the given cell type
-cell_data = cell_data[cell_data[snakemake.params.pseudobulk_param] == snakemake.params.cell_type]
+cell_data = cell_data[(cell_data['cell_type'] == snakemake.params.cell_type) & (cell_data[disease_param].isin(disease_list))]
 
+# Make sure there is a barcode column
 cell_data['barcode'] = [x.split('_')[0] for x in cell_data.index]
+# Create a DataFrame column with the sample_id name, required for downstream analysis
 cell_data['sample_id'] = cell_data[sample_key]
+# Required for sample organization by batches
 cell_sample_batch = cell_data[['sample_id', seq_batch_key]].drop_duplicates()
 
-# Get fragment files and corresponding samples
+# Link each fragment file (value) with a sample_id (key)
 fragment_files = snakemake.input.fragment_files
 samples = snakemake.params.samples
-fragment_dict = [str(x) for x in dict(zip(samples, fragment_files))]
+fragment_dict = dict(zip(samples, fragment_files))
 
-cell_type_samples = [str(x) for x in cell_sample_batch['sample_id'].to_list()]
+# Convert to sample_id values to a list comprehension
+cell_type_samples = [x for x in cell_sample_batch['sample_id'].to_list()]
+# 
 cell_type_fragments = {key: fragment_dict[key] for key in cell_type_samples}
 
+# Create list to store filtered adatas in
 adatas = []
 print(f'Iterating through {len(cell_type_samples)} samples')
 for sample, fragment_file in cell_type_fragments.items():
+    # Convert the fragment bed file into 
     cistopic_obj = create_cistopic_object_from_fragments(path_to_fragments=fragment_file,
                                                path_to_regions=snakemake.input.cell_bedfile,
                                                valid_bc = cell_data[cell_data['sample_id'] == sample]['barcode'].to_list(),
@@ -57,8 +70,9 @@ for sample, fragment_file in cell_type_fragments.items():
     adatas.append(adata)
     print(f"Done with sample {sample}")
 
+# Create an anndata object by concatenating cell/nuclei
 adata = ad.concat(join='outer', adatas=adatas)
-
+#adata.write_h5ad(f'/data/CARD_singlecell/SN_atlas/data/celltypes/{snakemake.params.cell_type}/{snakemake.params.cell_type}_')
 adata.var['chromosome'] = [x.split(':')[0] for x in adata.var.index]
 adata.var['start'] = [x.split(':')[1].split('-')[0] for x in adata.var.index]
 adata.var['end'] = [x.split(':')[1].split('-')[1] for x in adata.var.index]
@@ -68,8 +82,9 @@ adata.obs['atlas_identifier'] = adata.obs['atlas_identifier'].astype('category')
 adata.obs_names = adata.obs['atlas_identifier'].astype(str).to_list()
 
 # Convert the cisTopic parameters that were saved as objects?
-adata.obs['cisTopic_nr_frag'] = adata.obs['cisTopic_nr_frag'].astype(int)
-adata.obs['cisTopic_nr_acc'] = adata.obs['cisTopic_nr_acc'].astype(int)
+for observation in ['cisTopic_nr_frag', 'cisTopic_nr_acc', ]:
+    adata.obs[observation] = adata.obs[observation].astype(int)
+
 adata.obs['atlas_identifier'] = adata.obs['atlas_identifier'].astype(str)
 
 # Add required covariate values
@@ -90,12 +105,13 @@ adata.var = pd.merge(
     left_index=True,
     right_index=True)
 
-adata.var['Annotation'] = adata.var['Annotation'].astype('category')
-adata.var['Detailed Annotation'] = adata.var['Detailed Annotation'].astype('category')
-adata.var['Gene Name  '] = adata.var['Gene Name'].astype('category')
-adata.var['Gene Type '] = adata.var['Gene Type'].astype('category')
-adata.var['start'] = adata.var['start'].astype(int)
-adata.var['end'] = adata.var['end'].astype(int)
+# Convert variable columns to categories
+for variable in ['Annotation', 'Detailed Annotation', 'Gene Name', 'Gene Type']:
+    adata.var[variable] = adata.var[variable].astype('category')
+
+for variable in ['start', 'end']:
+    adata.var[variable] = adata.var[variable].astype(int)
+
 adata.var['total counts'] = np.array(adata.X.sum(axis=0))[0]
 adata.var['Simple Annotation'] = [str(x).split(' ')[0] for x in adata.var['Annotation']]
 adata.obs['cell_type'] = snakemake.params.cell_type
