d146288e6d4f17e8ff80b44b8b1b7f9c580fbceb
Adam Catching <adamcatching@gmail.com>
2026-06-19 09:36:07 -0400

    Update atac_preprocess.py

--- scripts/atac_preprocess.py
+++ scripts/atac_preprocess.py
@@ -1,8 +1,4 @@
-import pandas as pd
-import scanpy as sc
-import anndata as ad
 import snapatac2 as snap
-import pandas as pd
 
 # Import and create AnnData object from fragment file
 adata = snap.pp.import_data(
@@ -16,10 +12,5 @@ adata = snap.pp.import_data(
 _ = snap.pl.frag_size_distr(adata, show=False)
 # Get the transcription start sites 
 snap.metrics.tsse(adata, snap.genome.hg38)
-
-# Add tiled bins across chromosomes, bin size of 500
-snap.pp.add_tile_matrix(adata, bin_size=500)
-# For this sample, determine the count and highly variable bins
-snap.pp.select_features(adata, n_features=250000)
 # As this is a read-write interface, the AnnData object can just be closed
 adata.close()
