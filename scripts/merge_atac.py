9d3b840482ba32ec5863136e4ec7413cbab9984f
Adam Catching <adamcatching@gmail.com>
2026-06-21 20:55:40 -0400

    Create merge_atac.py

---
+++ scripts/merge_atac.py
@@ -0,0 +1,20 @@
+import anndata as ad
+import pandas as pd
+import scanpy as sc
+import scipy
+
+# Create dictionary of sample name to file, then filter with relevant diagnosis
+sample_loc = dict(zip(snakemake.params.samples, snakemake.input.atac_anndata))
+adatas = {key: sc.read_h5ad(sample_loc[key]) for key in snakemake.params.samples}
+
+# Concatenate with names of sample
+adata = ad.concat(
+    merge='same', index_unique='_', join='outer',
+    adatas=adatas
+    )
+print(adata)
+# Write out the unfiltered dataset
+adata.write_h5ad(
+    filename=snakemake.output.merged_atac_anndata, 
+    compression='gzip'
+    )
