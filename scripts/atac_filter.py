c760d8517762decdd421e4419ec6b4294a36051c
Adam Catching <adamcatching@gmail.com>
2026-06-21 20:59:30 -0400

    Filter on number of fragments and TSSE

--- scripts/atac_filter.py
+++ scripts/atac_filter.py
@@ -6,8 +6,6 @@ import snapatac2 as snap
 # Important that ATAC is read in using snapATAC2
 atac = snap.read(snakemake.input.atac_anndata, backed=None) 
 # Filter cells with less than 1000 counts
-snap.pp.filter_cells(atac, min_counts=snakemake.params.min_tsse)
-
-atac = atac[atac.obs['tsse'] > snakemake.params.min_tsse].copy()
+atac = atac[((atac.obs['n_fragment'] > snakemake.params.min_peak_counts) & (atac.obs['tsse'] > snakemake.params.min_tsse))].copy()
 
 atac.write(snakemake.output.atac_anndata, compression='gzip')
