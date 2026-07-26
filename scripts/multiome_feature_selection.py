253cd1348b54e0b28cdb55723f0f7679a5ac5cd6
Adam Catching <adamcatching@gmail.com>
2026-07-26 11:11:00 -0400

    Create multiome_feature_selection.py

---
+++ scripts/multiome_feature_selection.py
@@ -0,0 +1,26 @@
+import anndata as ad
+import scanpy as sc
+import torch
+import pandas as pd
+import scipy
+import numpy as np
+import muon as mu
+import snapatac2 as snap
+
+# Read in AnnData atlas object
+mdata = mu.read(snakemake.input.multiome_object)
+
+# Select for the most variable genes
+sc.pp.highly_variable_genes(
+    mdata["rna"], 
+    n_top_genes=snakemake.params.hvg,
+    subset=False)
+rna_hvg = mdata["rna"][:, mdata["rna"].var['highly_variable']].copy()
+
+snap.pp.select_features(mdata["atac"], n_features=snakemake.params.hvp, inplace=False)
+atac_hvp = mdata["atac"][:, mdata["atac"].var["selected"].values].copy()
+
+# Save the anndata object
+mdata = mu.MuData({"rna": rna_hvg, "atac": atac_hvp})
+
+mdata.write(snakemake.output.multiome_object)
